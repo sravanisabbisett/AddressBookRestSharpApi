a6b4590505d05af0beddcd62a70e06aecaee40be
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-20 15:37:10 +0530

    ADDED:POCO class for personData

---
+++ AddressBookRestSharpAPI/AddressBookTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddressBookRestSharpAPI
+{
+    [TestClass]
+    public class AddressBookTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ AddressBookRestSharpAPI/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookRestSharpAPI
+{
+    public class Person
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public int mobileNumber { get; set; }
+        public int id { get; set; }
+    }
+}
